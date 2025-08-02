a3aac181118e4cd22b7f7eaf7a8836f9e1766625
Amru Sajahan <135305295+amrusajahan@users.noreply.github.com>
2025-08-02 09:46:41 +0530

    added - dashboard link to navbar and improved authentication check in DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+
+        if (!Auth::check()) {
+            return redirect('/login')->with('status', 'Please log in to access the dashboard.');
+        }
+        
         return view('dashboard.index');
     }
 }
